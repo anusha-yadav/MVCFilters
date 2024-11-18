d5d807df0691aefc838efb80d226c287b643b547
anusha-yadav <anushayadav.1904f1@gmail.com>
2024-11-18 21:58:17 +0530

    made changes

---
+++ LibraryManagement/Program.cs
@@ -0,0 +1,65 @@
+using LibraryManagement.Data;
+using LibraryManagement.Filters;
+using LibraryManagement.Repository.Contracts;
+using LibraryManagement.Repository.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<LoggingFilter>();
+    options.Filters.Add<ExceptionFilter>();
+});
+
+builder.Services.AddAuthentication().AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddDbContext<EmployeeDBContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDB")));
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<LoggingFilter>();
+builder.Services.AddScoped<ExceptionFilter>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
